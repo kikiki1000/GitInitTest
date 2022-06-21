81bc7bcb47c158925ae79948e2922a3c83fe9e09
Ki-CheonChoi <ckc3425@gmail.com>
2022-06-21 22:47:51 +0900

    커밋

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("메인 수정도 몰래 했지롱");
-        System.out.println("새로운 기능 충돌용");
+        System.out.println("새로운 기능");
 
     }
 }
