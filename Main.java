29a80e46f117b98caa09d6598533f0452dab963b
Ki-CheonChoi <ckc3425@gmail.com>
2022-06-21 23:13:48 +0900

    VS code에서 커밋

--- Main.java
+++ Main.java
@@ -5,5 +5,12 @@ public class Main {
         System.out.println("메인 수정도 몰래 했지롱");
         System.out.println("새로운 기능");
 
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(j + "*" + i + "=" + i * j + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
