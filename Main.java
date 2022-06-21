9cbc82c1bb83762d9d0542c1b6d843c7a951b894
Ki-CheonChoi <ckc3425@gmail.com>
2022-06-21 22:26:55 +0900

    충돌 시험용 커밋

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("메인 수정");
-        System.out.println("새로운 기능");
+        System.out.println("메인 수정도 몰래 했지롱");
+        System.out.println("새로운 기능 충돌용");
     }
 }
